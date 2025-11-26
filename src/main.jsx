239ea6ef37dedda5a6d1fcf4a2b7ccd9777d2802
shreyanshs31 <shreyansh.shukla.3101@gmail.com>
2025-11-26 20:22:59 +0530

    added versel speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
